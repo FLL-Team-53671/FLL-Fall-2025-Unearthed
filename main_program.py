44097b24b4322f6b9ef3e2f485c671d9d42418de
Andrew McNabb <amcnabb@mcnabbs.org>
2025-11-02 15:19:04 -0500

    Merge main_hubmenu.py into main_program.py

--- main_program.py
+++ main_program.py
@@ -1,30 +1,17 @@
+from pybricks.tools import hub_menu
+import mission12
+import sample_mission1
+import sample_mission2
+from advanced_base_robot import *
 from base_robot import *
 
-# Import missions
-import Collecting_5_Bricks, sample_mission2
-
-
-br = BaseRobot()
-
-pressed = br.hub.buttons.pressed()
-
-# here's one way to create a master program. There are many other ways to do
-# it, but that is up to the team.
-# For example, here's another way:
-# https://pybricks.com/project/spike-hub-menu/
-
-# Here, we just wait until a button is pressed before starting the mission
-while Button.LEFT not in pressed:
-    pressed = br.hub.buttons.pressed()
-
-Collecting_5_Bricks.Run(br)
-
-# presumably the robot returned to base, and you have now configured it to
-# run the next mission. In any case, when sample_mission1 is done running
-# the robot goes again into the loop to wait for the button press. This
-# time we will run the sample_mission2 program
-
-while Button.LEFT not in pressed:
-    pressed = br.hub.buttons.pressed()
-
-sample_mission2.Run(br)
+program = hub_menu("a", "b", "c")
+if program == "a":
+    br = AdvancedBaseRobot()
+    mission12.Run(br)
+if program == "b":
+    br = BaseRobot()
+    sample_mission1.Run(br)
+if program == "c":
+    br = BaseRobot()
+    sample_mission2.Run(br)
