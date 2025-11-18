b6552384de20a2dbad688830bad099b9af074c17
Cobra Bytes Shared <amcnabb-github-fll@mcnabbs.org>
2025-11-18 18:43:52 -0500

    Changing the menu

--- master_program.py
+++ master_program.py
@@ -4,20 +4,18 @@ import melissa
 import mission12_part1
 import mission12_part2
 import sample_mission1
-import sample_mission2
+import mission6_and_5
 import xbox_remote_control
 from current_robot import current_robot
 
-
-program = hub_menu("a", "b", "c", "x")
-r = current_robot()
-if program == "a":
-    mission12_part1.Run(r)
-elif program == "b":
-    mission12_part2.Run(r)
-elif program == "c":
-    sample_mission2.Run(r)
-elif program == "M":
-    melissa.Run(r)
-elif program == "x":
-    xbox_remote_control.run(r)
+while True:
+    program = hub_menu("1", "2", "3", "x")
+    r = current_robot()
+    if program == "1":
+        mission12_part1.Run(r)
+    elif program == "2":
+        mission12_part2.Run(r)
+    elif program == "3":
+        mission6_and_5.Run(r)
+    elif program == "x":
+        xbox_remote_control.run(r)
