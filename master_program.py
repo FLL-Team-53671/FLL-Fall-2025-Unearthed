d102cfe8a88d8851bbe916a9a322e3c54d4e3c84
Zach Pezzementi <zach.pezzementi@gmail.com>
2025-12-13 14:03:21 -0500

    Added mission 9 and raise the roof

--- master_program.py
+++ master_program.py
@@ -7,9 +7,11 @@ import sample_mission1
 import mission6_and_5
 import xbox_remote_control
 from current_robot import current_robot
+import mission_raise_the_roof
+import mission9
 
 while True:
-    program = hub_menu("1", "2", "3", "x", "A")
+    program = hub_menu("1", "2", "3", "4", "5", "x", "A")
     r = current_robot()
     if program == "1":
         mission12_part1.Run(r)
@@ -17,7 +19,12 @@ while True:
         mission12_part2.Run(r)
     elif program == "3":
         mission6_and_5.Run(r)
+    elif program =="4":
+        mission_raise_the_roof.Run(r)
+    elif program =="5":
+        mission9.Run(r)
     elif program == "x":
         xbox_remote_control.run(r)
     elif program == "A":
         go_across_the_board.Run(r)
+    
--- mission_raise_the_roof.py
+++ mission_raise_the_roof.py
@@ -1,22 +1,26 @@
 from base_robot import BaseRobot
 from current_robot import current_robot
 
-# This is a mission program. You will have one of these for each "mission"
-# Normally a mission is one run from base, but there are ways to do more than
-# that if needed.
-
-# raise the roof Mission
-
-# . make sure cuddles is on the wall to start make the right castor right in front of the theta(o with a line in the middle)
 
 
 def Run(br: BaseRobot):
-    br.driveForDistance(670, 200)
-    br.robot.arc(-115, 50)
-    br.robot.arc(-115, -50)
-    br.driveForDistance(-670, 200)
+    br.driveForDistance(625, 200)
+    br.robot.arc(-150, 120)
+    br.driveForDistance(50, 200)
+    br.robot.turn(-90)
+    br.driveForDistance(60, 200)
+    br.driveForDistance(-60, 200)
+    br.robot.turn(90)
+    br.driveForDistance(-50, 200)
+    br.robot.arc(-150, -120)
+    br.driveForDistance(-625, 200)
+    
+    
+    
+    
+
 
 
 if __name__ == "__main__":
-    br = current_robot()
-    Run(br)
+    r = current_robot()
+    Run(r)
